68ea1ffbd322619ce90341cd27805748b3617cda
Kevin Oh <aflashyrhetoric@gmail.com>
2022-05-05 01:17:32 -0400

    Update some types

--- src/api/kit/controllers/kit.js
+++ src/api/kit/controllers/kit.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ *  kit controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::kit.kit');
